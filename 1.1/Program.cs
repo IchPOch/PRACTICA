55200b327a423067ada752bab727cfebc3d2a4a0
Миша Епринцев <meprincev@bk.ru>
2020-05-04 00:37:09 +0300

    123

---
+++ 1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 1 / (2 * Math.PI);
+            Console.WriteLine("Длина окруж = 1");
+            Console.WriteLine("Радиус = " + r);
+        }
+    }
+
+}
